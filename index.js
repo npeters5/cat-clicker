d89b5e48d8ee9aa3d497640ec56a5ae17b98e90a
Nora Peters <norapeters5@gmail.com>
2017-09-27 21:23:04 -0400

    finalize model, view, controller

--- index.js
+++ index.js
@@ -14,26 +14,26 @@ const octopus = {
     //Set current cat to the first one in the list
     model.currentCat = model.cats[0];
 
-      //initialize views
+    //initialize views
     catView.init();
     catListView.init();
   },
   //getter for model.cats
   get cats() {
-      return model.cats;
+    return model.cats;
   },
   //getter for model.currentCat
   get currentCat() {
-      return model.currentCat;
+    return model.currentCat;
   },
   //setter for currentCat
-  set currentCat() {
-      model.currentCat = cat;
+  set currentCat(cat) {
+    model.currentCat = cat;
   },
-  //increment counter then reader
+  //increment counter then render
   incrementCounter() {
-      model.currentCat.clickCount++;
-      catView.render();
+    model.currentCat.clickCount++;
+    catView.render();
   }
 };
 
@@ -41,15 +41,56 @@ const octopus = {
 
 const catView = {
   init() {
-      //store pointers
+    //store pointers to DOM elements for later access
+    this.catEl = document.querySelector(".cat");
+    this.catNameEl = document.querySelector(".cat-name");
+    this.catImgEl = document.querySelector(".cat-img");
+    this.countEl = document.querySelector(".cat-count");
+
+    //on click, incremenet cat's counter
+    this.catImgEl.addEventListener("click", () => octopus.incrementCounter());
+
+    //render this view (update DOM elements)
+    this.render();
+  },
+  render() {
+    //update DOM elements with values from current cat
+    console.log("hello");
+    const currentCat = octopus.currentCat;
+    this.catNameEl.innerText = currentCat.name;
+    this.catImgEl.src = currentCat.imgSrc;
+    this.countEl.innerText = currentCat.clickCount;
   }
 };
 
 const catListView = {
-    init() {
-        //store
-    }
-    render() {
+  init() {
+    //store DOM elements for later access
+    this.catListEl = document.getElementById("cat-list");
+    const cats = octopus.cats;
 
+    //loops over cats
+    for (const cat of cats) {
+      const option = document.createElement("option");
+      option.value = cat.name;
+      option.innerText = cat.name;
+      this.catListEl.appendChild(option);
     }
-}
+
+    //add listener on select to change current cat and render
+    this.catListEl.addEventListener("change", e => {
+      const cat = cats.find(cat => cat.name === e.target.value);
+
+      octopus.currentCat = cat;
+      catView.render();
+    });
+
+    //render this view (update DOM elements)
+    this.render();
+  },
+  render() {
+    //get the cats from the octopus
+  }
+};
+
+octopus.init();
